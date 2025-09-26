66ed7e528db6d7ab83c192709b7b8b14df62bfbd
diyorbekmuhammadiev <diyorbek.programmer@gmail.com>
2025-09-26 17:04:30 +0500

    bug fixed

--- ios/Classes/Scanner Core/ScanProcessor.swift
+++ ios/Classes/Scanner Core/ScanProcessor.swift
@@ -3,11 +3,10 @@
 //  Card ScanProcessor
 //
 //  Created by Mohammed Sadiq on 26/07/20.
-//  Copyright © 2020 MZaink. All rights reserved.
+//  Updated by <siz> on 2025
 //
 
 import UIKit
-//import MLKitTextRecognition
 import GoogleMLKit
 
 public protocol ScanProcessorDelegate {
@@ -17,19 +16,19 @@ public protocol ScanProcessorDelegate {
 public class ScanProcessor {
     var scanProcessorDelegate: ScanProcessorDelegate?
     var card: CardDetails = CardDetails()
-    
+
     var datesCollectedSoFar: [String] = []
     var validScansSoFar: Int = 0
     var singleFrameCardScanner: SingleFrameCardScanner
     var cardDetailsScanOptimizer: CardDetailsScanOptimizer
     var cardScanOptions: CardScannerOptions
-    
+
     init(withOptions cardScanOptions: CardScannerOptions) {
         self.cardScanOptions = cardScanOptions
         self.singleFrameCardScanner = SingleFrameCardScanner(withOptions: cardScanOptions)
         self.cardDetailsScanOptimizer = CardDetailsScanOptimizer(scannerOptions: cardScanOptions)
     }
-    
+
     func startScanning() {
         let cameraViewController: CameraViewController = makeCameraViewController()
         UIApplication.shared.keyWindow?.rootViewController?.present(
@@ -38,48 +37,59 @@ public class ScanProcessor {
             completion: nil
         )
     }
-    
+
     func makeCameraViewController() -> CameraViewController {
         let cameraViewController: CameraViewController = CameraViewController()
         cameraViewController.cameraDelegate = self
         cameraViewController.cameraOrientation = cardScanOptions.cameraOrientation
         cameraViewController.prompt = cardScanOptions.prompt
         cameraViewController.modalPresentationStyle = .fullScreen
-        
+
         return cameraViewController
     }
 }
 
-// MARK:- CameraDelegate
+// MARK: - CameraDelegate
 extension ScanProcessor: CameraDelegate {
-    func camera(_ camera: CameraViewController, didScan scanResult: Text) {
-        guard let cardDetails = singleFrameCardScanner.scanSingleFrame(visionText: scanResult) else {
-            return
-        }
-        
-        cardDetailsScanOptimizer.processCardDetails(cardDetails: cardDetails)
-    
-        if (cardDetailsScanOptimizer.isReadyToFinishScan()) {
-            vibrateToIndicateScanEnd()
-            
-            card.cardNumber = cardDetailsScanOptimizer.getOptimalCardDetails()?.cardNumber ?? ""
-            card.cardHolderName = cardDetailsScanOptimizer.getOptimalCardDetails()?.cardHolderName ?? ""
-            card.expiryDate = cardDetailsScanOptimizer.getOptimalCardDetails()?.expiryDate ?? ""
-            
-            scanProcessorDelegate?.scanProcessor(self, didFinishScanning: card)
-            camera.stopScanning()
+    func camera(_ camera: CameraViewController, didCaptureImage image: UIImage) {
+        let visionImage = VisionImage(image: image)
+        visionImage.orientation = image.imageOrientation
+
+        let textRecognizer = TextRecognizer.textRecognizer()
+
+        textRecognizer.process(visionImage) { visionText, error in
+            guard error == nil, let visionText = visionText else {
+                return
+            }
+
+            // Single-frame scanner bilan ishlash
+            guard let cardDetails = self.singleFrameCardScanner.scanSingleFrame(visionText: visionText) else {
+                return
+            }
+
+            self.cardDetailsScanOptimizer.processCardDetails(cardDetails: cardDetails)
+
+            if self.cardDetailsScanOptimizer.isReadyToFinishScan() {
+                self.vibrateToIndicateScanEnd()
+
+                self.card.cardNumber = self.cardDetailsScanOptimizer.getOptimalCardDetails()?.cardNumber ?? ""
+                self.card.cardHolderName = self.cardDetailsScanOptimizer.getOptimalCardDetails()?.cardHolderName ?? ""
+                self.card.expiryDate = self.cardDetailsScanOptimizer.getOptimalCardDetails()?.expiryDate ?? ""
+
+                self.scanProcessorDelegate?.scanProcessor(self, didFinishScanning: self.card)
+                camera.stopScanning()
+            }
         }
     }
-    
+
     func vibrateToIndicateScanEnd() {
         let impactFeedbackgenerator = UIImpactFeedbackGenerator(style: .heavy)
         impactFeedbackgenerator.prepare()
         impactFeedbackgenerator.impactOccurred()
     }
-    
+
     func cameraDidStopScanning(_ camera: CameraViewController) {
-        if (cardDetailsScanOptimizer.isReadyToFinishScan()) {
-            // Delegate back to Flutter from here
+        if cardDetailsScanOptimizer.isReadyToFinishScan() {
             scanProcessorDelegate?.scanProcessor(self, didFinishScanning: card)
         }
     }
